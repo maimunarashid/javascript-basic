77204726113d4c9ea7ebfb2ad88c823a8a9930a7
maimunarashid <rashidromana05@gmail.com>
2025-10-26 23:30:02 +0600

    parseInt() file

---
+++ parseInt.js
@@ -0,0 +1,37 @@
+// number: output 60
+let applePrice = 20;
+let orrangePrice = 40;
+let price = applePrice + orrangePrice;
+console.log(price);
+
+//string: output 2040
+let applePrice1 = '20';
+let orrangePrice1 = '40';
+let price1 = applePrice1 + orrangePrice1;
+console.log(price1);
+
+// number and string addition: output 2040
+let applePrice2 = '20';
+let orrangePrice2 = 40;
+let price2 = applePrice2 + orrangePrice2;
+console.log(price2);
+
+// number and string addition: output 2040
+let applePrice3 = 20;
+let orrangePrice3 = '40';
+let price3 = applePrice3 + orrangePrice3;
+console.log(price3);
+
+// solution of number and string addition by convert string in int by parseInt: output 60
+let applePrice4 = parseInt('20');
+let orrangePrice4 = 40;
+let price4 = applePrice4 + orrangePrice4;
+console.log(price4);
+
+// Output: NaN (Not a Number, if the string does not consist of numeric characters, parseInt() will return NaN)
+let applePrice5 = 20;
+let amla = parseInt('amla');
+let price5 = applePrice5 + amla;
+console.log(price5);
+
+// Adding a number and a non-numeric string result: string concatenation. Example: 20 + 'amla' = '20amla'
---
+++ readme.md
@@ -0,0 +1,8 @@
+<!-- parseInt() -->
+1. When adding a number (int) and a string, a problem occurs.
+2. For example, it does not add 20 and '40' as numbers.
+3. Instead, it outputs '2040' a string concatenation.
+4. To perform actual addition, convert the string to a number using parseInt().
+5. See practical implementation in the 'parseInt.js' file.
+6. if the string does not consist of numeric characters, parseInt() will return NaN
+7. Adding a number and a non-numeric string result: string concatenation. Example: 20 + 'amla' = '20amla'
